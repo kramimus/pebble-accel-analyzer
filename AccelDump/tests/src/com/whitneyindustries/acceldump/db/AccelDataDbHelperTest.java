43fccd1cbfae3270dbb4bd0e0edccef9f3ff472c
Mark G. Whitney <markwhitney@gmail.com>
2014-03-25 22:55:25 -0400

    tests for db boilerplate
    - checking that RenamingDelegatingContext does what it says

---
+++ AccelDump/tests/src/com/whitneyindustries/acceldump/db/AccelDataDbHelperTest.java
@@ -0,0 +1,43 @@
+package com.whitneyindustries.acceldump.db;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import com.whitneyindustries.acceldump.db.AccelDataContract.QueuedMessageEntry;
+
+public class AccelDataDbHelperTest extends AndroidTestCase {
+    private static final String TEST_FILE_PREFIX = "test_";
+
+    private AccelDataDbHelper mDbHelper;
+    private SQLiteDatabase db;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), TEST_FILE_PREFIX);
+        mDbHelper = new AccelDataDbHelper(context);
+        db = mDbHelper.getWritableDatabase();
+    }
+
+    public void testWriteRead() throws Exception {
+        ContentValues values = new ContentValues();
+        String msgIn = "test message";
+
+        values.put(QueuedMessageEntry.COLUMN_NAME_GEN_TIME, System.currentTimeMillis());
+        values.put(QueuedMessageEntry.COLUMN_NAME_MESSAGE, msgIn);
+        values.put(QueuedMessageEntry.COLUMN_NAME_RETRIES, 0);
+
+        long newRowId = db.insert(QueuedMessageEntry.TABLE_NAME, null, values);
+
+        assertTrue(newRowId > 0);
+
+        Cursor c = db.query(false, QueuedMessageEntry.TABLE_NAME, null, null, null, null, null, null, null);
+        c.moveToFirst();
+        String msgOut = c.getString(c.getColumnIndexOrThrow(QueuedMessageEntry.COLUMN_NAME_MESSAGE));
+        assertEquals(msgIn, msgOut);
+    }
+}
+
