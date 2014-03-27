26ae48527f4a6d8396eb9992fa567bcc85b553e8
Mark G. Whitney <markwhitney@gmail.com>
2014-03-27 00:04:29 -0400

    another test for db boilerplate

--- AccelDump/tests/src/com/whitneyindustries/acceldump/db/AccelDataDbHelperTest.java
+++ AccelDump/tests/src/com/whitneyindustries/acceldump/db/AccelDataDbHelperTest.java
@@ -39,5 +39,26 @@ public class AccelDataDbHelperTest extends AndroidTestCase {
         String msgOut = c.getString(c.getColumnIndexOrThrow(QueuedMessageEntry.COLUMN_NAME_MESSAGE));
         assertEquals(msgIn, msgOut);
     }
+
+    public void testWriteDelete() throws Exception {
+        ContentValues values = new ContentValues();
+        String msgIn = "test message";
+
+        values.put(QueuedMessageEntry.COLUMN_NAME_GEN_TIME, System.currentTimeMillis());
+        values.put(QueuedMessageEntry.COLUMN_NAME_MESSAGE, msgIn);
+        values.put(QueuedMessageEntry.COLUMN_NAME_RETRIES, 0);
+
+        long newRowId = db.insert(QueuedMessageEntry.TABLE_NAME, null, values);
+
+        assertTrue(newRowId > 0);
+
+        String selection = QueuedMessageEntry.COLUMN_NAME_MESSAGE + " LIKE ?";
+        String[] selectionArgs = {msgIn};
+        db.delete(QueuedMessageEntry.TABLE_NAME, selection, selectionArgs);
+
+        Cursor c = db.query(false, QueuedMessageEntry.TABLE_NAME, null, null, null, null, null, null, null);
+        c.moveToFirst();
+        assertEquals(0, c.getCount());
+    }
 }
 
