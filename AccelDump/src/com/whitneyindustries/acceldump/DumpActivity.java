2e59aae20ecf86c6e800eb005ab68dec6291e04c
Mark G. Whitney <markwhitney@gmail.com>
2014-03-31 23:35:13 -0400

    start UI for IP address seting

--- AccelDump/src/com/whitneyindustries/acceldump/DumpActivity.java
+++ AccelDump/src/com/whitneyindustries/acceldump/DumpActivity.java
@@ -5,10 +5,15 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DumpActivity extends Activity
@@ -19,12 +24,29 @@ public class DumpActivity extends Activity
 
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
+    private EditText mIpField;
+    private Button mIpSave;
+
+    private OnClickListener mIpSaveListener = new OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getPreferences(Context.MODE_PRIVATE).edit();
+                editor.putString(getString(R.string.server_ip), mIpField.getText().toString());
+                editor.commit();
+            }
+        };
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        String ip = getPreferences(Context.MODE_PRIVATE).getString(getString(R.string.server_ip), "127.0.0.1");
+        mIpField = (EditText)findViewById(R.id.ip_field);
+        mIpField.setText(ip);
+        mIpSave = (Button)findViewById(R.id.ip_save);
+        mIpSave.setOnClickListener(mIpSaveListener);
+
 
         alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, DataPostReceiver.class);
