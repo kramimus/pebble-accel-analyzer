001c9a2bd56cedfa7664a1e2b1f981e5cb19012d
Mark G. Whitney <markwhitney@gmail.com>
2014-05-30 00:24:26 -0400

    basic auth on the readings collector server side

--- accel_sink/http2mongo.py
+++ accel_sink/http2mongo.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-from flask import Flask, request
+from basic_auth import requires_auth
+from cube import Cube
+from datetime import datetime
+from flask import Flask, request, abort
 import pymongo
 
 EXPECTED_ATTRS = ('x', 'y', 'z', 'v', 'ts')
 app = Flask(__name__)
 watch_data = pymongo.MongoClient()['watch_data']
+cube = Cube()
 
 @app.route('/', methods=['POST'])
+@requires_auth
 def collect_accel():
     readings = request.get_json()
 
@@ -22,8 +27,17 @@ def collect_accel():
     watch_data.accel_data.insert({'start_ts': start_ts,
                                   'end_ts': end_ts,
                                   'readings': good_readings})
+    print datetime.utcfromtimestamp(start_ts / 1000.0), "-", datetime.utcfromtimestamp(end_ts / 1000.0)
+
+    for r in good_readings:
+        isots = datetime.utcfromtimestamp(r['ts'] / 1000.0)
+        cube.put("accel", {'x': r['x'], 'y': r['y'], 'z': r['z']}, time=isots.isoformat())
+
     return "ok"
 
+@app.route('/', methods=['GET'])
+def check_ok():
+    return "ok"
 
 if __name__ == '__main__':
     watch_data.accel_data.ensure_index('start_ts')
